9fef655fa4ca2095e68eb835aabba666195325c9
saransh chaudhary <102319329+hackersara@users.noreply.github.com>
2024-06-03 20:03:34 +0530

    Create bubblesort_(optimized).cpp

---
+++ bubblesort_(optimized).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void bubblesort(int arr[],int n){
+bool swapped;
+ for(int count=1;count<n;count++){ 
+    for(int i=0;i<n-count;i++){ 
+        if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+            swapped=1;
+        }
+    }
+    if(swapped==1){
+        break;
+    }
+  }
+}
+
+void printarr(int arr[],int n ){
+// [-10 -4 3 9 31 50 51 100]
+ for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+ }
+}
+
+
+int main(){
+ int arr[]= {1,2,3,4,5,6,7};
+ int  n= sizeof(arr)/sizeof(int);
+ 
+ bubblesort(arr,n);
+ printarr(arr,n);
+
+}
